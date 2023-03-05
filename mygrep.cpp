2424f550b37af6c4437d9c16ae6b306d76ce05d8
Lassi Keijonen <lassi.keijonen@tuni.fi>
2023-03-05 20:18:28 +0200

    modified

--- mygrep.cpp
+++ mygrep.cpp
@@ -3,27 +3,35 @@
 #include <fstream>
 using namespace std;
 
-int main(int argc, char*argv[]) {
-    if(argc == 1) {
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
         cout << "Give a string from which to search for: ";
         string input;
-        cin >> input;
+        getline(cin, input);
         cout << "Give search string: ";
         string search;
-        cin >> search;
-        size_t pos=input.find(search); 
-        if(pos != string::npos) {
+        getline(cin, search);
+        size_t pos = input.find(search);
+        if (pos != string::npos)
+        {
             cout << '"' << search << "\" found in \"" << input << "\" in position " << pos << endl;
-        } else {
+        }
+        else
+        {
             cout << '"' << search << "\" NOT found in \"" << input << '"' << endl;
         }
     }
-    else if(argc==3) {
+    else if (argc == 3)
+    {
         ifstream file(argv[2]);
         string line;
         string search = argv[1];
-        while(getline(file,line)) {
-            if(line.find(search)!= string::npos) {
+        while (getline(file, line))
+        {
+            if (line.find(search) != string::npos)
+            {
                 cout << line << endl;
             }
         }
